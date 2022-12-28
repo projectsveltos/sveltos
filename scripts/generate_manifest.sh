1d3b029338e06429d742e1f5a0ce5b630b1eb61c
gianlucm76 <gianluca.mardente@gmail.com>
2022-12-28 10:02:02 +0100

    Add scripts
    1. generate_manifest.sh builds manifest for a given branch;
    1. upload_docker_images.sh builds and uploads docker images for a given
    branch.
    
    Those scripts are meant to be invoked only by Makefile targets.

---
+++ scripts/generate_manifest.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# DO NOT INVOKE DIRECTLY. Use Makefile target generate-manifest
+
+branch=${1}
+
+echo "Generate manifest for branch ${branch}"
+
+rm -rf  manifest/manifest.yaml
+touch  manifest/manifest.yaml
+
+# libsveltos
+echo "processing libsveltos"
+rm -rf tmp; mkdir tmp; cd tmp
+git clone git@github.com:projectsveltos/libsveltos.git
+cd libsveltos
+git checkout ${branch}
+for f in config/crd/bases/*.yaml
+do 
+    echo "Processing $f file..."
+    cat $f >> ../../manifest/manifest.yaml
+    echo "---"  >> ../../manifest/manifest.yaml 
+done
+cd ../../; rm -rf tmp
+
+# sveltos-manager
+echo "processing sveltos-manager"
+rm -rf tmp; mkdir tmp; cd tmp
+git clone git@github.com:projectsveltos/sveltos-manager.git
+cd sveltos-manager
+git checkout ${branch}
+for f in manifest/*.yaml
+do 
+    echo "Processing $f file..."
+    cat $f >> ../../manifest/manifest.yaml
+    echo "---"  >> ../../manifest/manifest.yaml 
+done
+cd ../../; rm -rf tmp
+
+# classifier
+echo "processing classifier"
+rm -rf tmp; mkdir tmp; cd tmp
+git clone git@github.com:projectsveltos/classifier.git
+cd classifier
+git checkout ${branch}
+for f in manifest/*.yaml
+do 
+    echo "Processing $f file..."
+    cat $f >> ../../manifest/manifest.yaml
+    echo "---"  >> ../../manifest/manifest.yaml 
+done
+cd ../../; rm -rf tmp
+
+# access-manager
+echo "processing access-manager"
+rm -rf tmp; mkdir tmp; cd tmp
+git clone git@github.com:projectsveltos/access-manager.git
+cd access-manager
+git checkout ${branch}
+for f in manifest/*.yaml
+do 
+    echo "Processing $f file..."
+    cat $f >> ../../manifest/manifest.yaml
+    echo "---"  >> ../../manifest/manifest.yaml 
+done
+cd ../../; rm -rf tmp
+
+# sveltoscluster-manager
+echo "processing sveltoscluster-manager"
+rm -rf tmp; mkdir tmp; cd tmp
+git clone git@github.com:projectsveltos/sveltoscluster-manager.git
+cd sveltoscluster-manager
+git checkout ${branch}
+for f in manifest/*.yaml
+do 
+    echo "Processing $f file..."
+    cat $f >> ../../manifest/manifest.yaml
+    echo "---"  >> ../../manifest/manifest.yaml 
+done
+cd ../../; rm -rf tmp
